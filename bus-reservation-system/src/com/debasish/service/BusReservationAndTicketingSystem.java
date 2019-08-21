9cd2d7d78a311fa38e0caa758e11a3cbb2c3448a
deba8523 <debasishsahoo8523@gmail.com>
2019-08-21 16:25:04 +0530

    added description

--- bus-reservation-system/src/com/debasish/service/BusReservationAndTicketingSystem.java
+++ bus-reservation-system/src/com/debasish/service/BusReservationAndTicketingSystem.java
@@ -1,7 +1,7 @@
 /**
  * @author Debasish Sahoo
- * @description This is a project where we will read three files i.e., bus_destination.txt, bus_fare.txt & Data.txt.
- * 				and we can authenticate user through username = 1, password = 1. Select the destinations and passengers by entering your choices.
+ * @description This is a project where we will read three files i.e., bus_destination.txt, bus_fare.txt & Data.txt
+ * 				We can authenticate user through username = 1, password = 1. Select the destinations and passengers by entering your choices.
  * 				You can see the bill how much is your fare cost as well as you can get some discounts.
  *
  */
